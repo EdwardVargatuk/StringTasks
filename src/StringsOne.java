8eb561a5c22b6b3034d9a9748e57211794dd4b54
EdwardVargatuk <44446341+EdwardVargatuk@users.noreply.github.com>
2018-11-28 22:33:28 +0200

    stream shit

---
+++ src/StringsOne.java
@@ -0,0 +1,132 @@
+import java.util.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class StringsOne {
+
+    private List<String> listOfWordsInFile = new ArrayList<>();
+    private Map<String, Long> treeMap;
+
+
+    private List<String> getListOfWordsInFile() {
+        return listOfWordsInFile;
+    }
+
+    private Map<String, Long> getTreeMap() {
+        return treeMap;
+    }
+
+    private void read(String words) {
+        Scanner reader = new Scanner(words);
+        while (reader.hasNext()) {
+            String str = reader.next().replaceAll("[,.\";:?!\\-+|\\[\\](){}`’']", "");
+            listOfWordsInFile.add(str);
+        }
+
+    }
+
+    private void countingWordsInText(List<String> words) {
+        List<String> list = new ArrayList<>();
+        Map<String, Long> treeMapWithout0;
+List <Boolean> b = new ArrayList<>();
+        listOfWordsInFile.forEach(nested -> words.stream().filter(nested::equalsIgnoreCase).forEach(list::add));
+
+//        Predicate<String> predicate = i->Map.Entry.getKey().equalsIgnoreCase(i));
+
+        treeMapWithout0 = list.stream().collect(Collectors.groupingBy(key -> key, Collectors.counting()));
+        System.out.println(treeMapWithout0);
+//        words.stream().filter(w -> !treeMapWithout0.containsKey(w)).forEach(x -> treeMapWithout0.put(x, (long) 0));
+
+
+//        treeMapWithout0.entrySet().forEach((nested,x) -> words.stream().filter(i->nested.equalsIgnoreCase(i)).forEach(System.out::println));
+        treeMap = treeMapWithout0.entrySet().stream()
+                .sorted(Map.Entry.comparingByValue(Collections.reverseOrder())).collect(Collectors.toMap(Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        (e1, e2) -> e2,
+                        LinkedHashMap::new));
+    }
+
+    private void sortWordsAccordingTheirFrequency(String words, List<String> forFrequency) {
+        read(words);
+        System.out.println(listOfWordsInFile);
+        countingWordsInText(forFrequency);
+        Map<String, Long> treeMap1 = getTreeMap();
+        treeMap1.forEach((key, value) -> System.out.println(key + " = " + value));
+    }
+
+    public static void main(String[] args) {
+        String text = "In Spring, the objects that form the backbone of your application and that are managed by the Spring IoC container are called beans. ";
+        StringsOne stringsOne = new StringsOne();
+        List<String> lists = new ArrayList<>();
+        lists.add("Intellij");
+        lists.add("and");
+        lists.add("THE");
+        lists.add("best");
+        lists.add("for");
+        lists.add("spring");
+        stringsOne.sortWordsAccordingTheirFrequency(text, lists);
+
+
+
+
+        //        for (String word : words) {
+//            if (!treeMapWithout0.containsKey(word))
+//
+//                treeMapWithout0.put(word, (long) 0);
+//        }
+//        System.out.println(treeMapWithout0.entrySet());
+
+
+        //      words.forEach(a->list.stream().filter(w->a.equalsIgnoreCase(w)).collect(Collectors.groupingBy(key -> key,  Collectors.counting() )).entrySet().forEach(System.out::println));
+        //        list= listOfWordsInFile.stream().filter(w->"s".equalsIgnoreCase(w)).collect(Collectors.toList());
+//        int bound = words.size();
+//        List<String> list = new ArrayList<>();
+//                for (String st : listOfWordsInFile) {
+//
+//                for (String strings : words) {
+//                    if (strings.equalsIgnoreCase(st))
+//                    list.add(st);
+//                }
+//        }
+//        Stream.concat(Stream.of(list), Stream.of(words)).forEach(System.out::println);
+
+
+        //            if (words.get(u).equalsIgnoreCase(w)) {
+//                list.add(words.get(u));
+//            }}
+//
+//        list=  listOfWordsInFile.stream().map(w->{
+//            List<String> list = new ArrayList<>();
+//            IntStream.range(0, words.size()).filter((u) -> words.get(u).equalsIgnoreCase(w))
+//                            .forEach(i->list.add(words.get(i)));
+//
+////list.add(w);
+//            return Stream.of(list);
+//        }).collect(Collectors.toList());
+//        System.out.println(words);
+//        System.out.println(list);
+//                .map(x->x)
+//        treeMap = words.stream().filter(l -> l.length() >= NUMBERGRAPH)
+//                .flatMap((x) -> {
+//                    List<String> list = new ArrayList<>();
+//                    IntStream.range(0, x.length() - (NUMBERGRAPH - 1))
+//                            .forEach((u) -> list.add(x.toUpperCase().substring(u, NUMBERGRAPH + u)));
+//                    return Stream.of(list);
+//                })
+//                .collect(Collectors.toList()).stream().flatMap(Collection::stream)
+//                .collect(Collectors.groupingBy(key -> key, TreeMap::new, Collectors.counting()));
+
+
+//
+//        System.out.println(lists);
+//
+//        stringsOne.countingWordsInText(lists);
+//        lists.add("spr");
+//        stringsOne.countingWordsInText(lists);
+
+
+    }
+}
+
